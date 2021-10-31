096bf74854a2ffb1a2fed0e5ab4af6c82ea67e10
Lukas Krickl <lukas@krickl.dev>
2021-10-31 09:17:26 +0100

    Working commit

--- lib/src/output.rs
+++ lib/src/output.rs
@@ -1,6 +1,4 @@
-use std::marker::PhantomData;
-
-use crate::{Promo, PromoState, Task, Timer};
+use crate::{Promo, PromoState, Task};
 
 pub struct PromoTransition {
     pub from: PromoState,
@@ -10,26 +8,6 @@ pub struct PromoTransition {
 impl PromoTransition {
     pub fn new(from: PromoState, to: PromoState) -> Self {
         Self { from, to }
-    }
-}
-
-pub struct PromoUpdate<'a, TTask, TTimer>
-where
-    TTask: Task,
-    TTimer: Timer,
-{
-    pub state: PromoState,
-    pub timer: Option<&'a TTimer>,
-    pub task: Option<&'a TTask>,
-}
-
-impl<'a, TTask, TTimer> PromoUpdate<'a, TTask, TTimer>
-where
-    TTask: Task,
-    TTimer: Timer,
-{
-    pub fn new(state: PromoState, timer: Option<&'a TTimer>, task: Option<&'a TTask>) -> Self {
-        Self { state, timer, task }
     }
 }
 
@@ -49,148 +27,10 @@ where
     }
 }
 
-pub enum PromoMessage<'a, TTask, TTimer>
+pub enum PromoMessage<TTask>
 where
     TTask: Task,
-    TTimer: Timer,
 {
     Transition(PromoTransition),
-    Update(PromoUpdate<'a, TTask, TTimer>),
     TaskCompleted(PromoTaskCompleted<TTask>),
-}
-
-/// This is a receiver for output
-/// This can be any system that can receive messages from
-/// promo about its current state
-pub trait OutputSystem<TTask, TTimer, TError>
-where
-    TTask: Task,
-    TTimer: Timer,
-{
-    /// received every time the promo is updated
-    fn update(
-        &mut self,
-        promo: &dyn Promo<TTask, TTimer, TError>,
-        state: PromoState,
-        timer: Option<&TTimer>,
-        task: Option<&TTask>,
-    ) -> Result<(), TError>;
-
-    fn task_completed(
-        &mut self,
-        promo: &dyn Promo<TTask, TTimer, TError>,
-        task: &TTask,
-    ) -> Result<(), TError>;
-
-    /// called for every state transition
-    fn state_changed(
-        &mut self,
-        promo: &dyn Promo<TTask, TTimer, TError>,
-        from: PromoState,
-        to: PromoState,
-    ) -> Result<(), TError>;
-}
-
-#[derive(Eq, PartialEq, Debug)]
-pub enum SampleOutputCallee {
-    Update,
-    StateChanged,
-    TaskCompelted,
-}
-
-#[derive(Debug, PartialEq)]
-pub struct SampleOutputData<TTask>
-where
-    TTask: Task + PartialEq,
-{
-    pub callee: SampleOutputCallee,
-    pub task: Option<TTask>,
-    pub state: PromoState,
-    pub from: Option<PromoState>,
-}
-
-// This is a sample output system
-pub struct SampleOutputSystem<TTask, TTimer, TError>
-where
-    TTask: Task + Clone + PartialEq,
-    TTimer: Timer + Clone,
-{
-    pub data: Vec<SampleOutputData<TTask>>, // contains a version of the output
-    phantom_task: PhantomData<TTask>,
-    phantom_timer: PhantomData<TTimer>,
-    phantom_error: PhantomData<TError>,
-}
-
-impl<TTask, TTimer, TError> Default for SampleOutputSystem<TTask, TTimer, TError>
-where
-    TTask: Task + Clone + PartialEq,
-    TTimer: Timer + Clone,
-{
-    fn default() -> Self {
-        Self {
-            data: vec![],
-            phantom_task: PhantomData::default(),
-            phantom_timer: PhantomData::default(),
-            phantom_error: PhantomData::default(),
-        }
-    }
-}
-
-impl<TTask, TTimer, TError> OutputSystem<TTask, TTimer, TError>
-    for SampleOutputSystem<TTask, TTimer, TError>
-where
-    TTask: Task + Clone + PartialEq,
-    TTimer: Timer + Clone,
-{
-    /// received every time the promo is updated
-    fn update(
-        &mut self,
-        _promo: &dyn Promo<TTask, TTimer, TError>,
-        state: PromoState,
-        _timer: Option<&TTimer>,
-        task: Option<&TTask>,
-    ) -> Result<(), TError> {
-        self.data.push(SampleOutputData {
-            callee: SampleOutputCallee::Update,
-            task: if let Some(task) = task {
-                Some(task.clone())
-            } else {
-                None
-            },
-            state,
-            from: None,
-        });
-
-        Ok(())
-    }
-
-    fn task_completed(
-        &mut self,
-        promo: &dyn Promo<TTask, TTimer, TError>,
-        task: &TTask,
-    ) -> Result<(), TError> {
-        self.data.push(SampleOutputData {
-            callee: SampleOutputCallee::TaskCompelted,
-            task: Some(task.clone()),
-            state: promo.state(),
-            from: None,
-        });
-        Ok(())
-    }
-
-    /// called for every state transition
-    fn state_changed(
-        &mut self,
-        _promo: &dyn Promo<TTask, TTimer, TError>,
-        from: PromoState,
-        to: PromoState,
-    ) -> Result<(), TError> {
-        self.data.push(SampleOutputData {
-            callee: SampleOutputCallee::Update,
-            task: None,
-            state: to,
-            from: Some(from),
-        });
-        Ok(())
-    }
 }
--- lib/src/promo.rs
+++ lib/src/promo.rs
@@ -1,35 +1,29 @@
-use std::marker::PhantomData;
-
-use crate::{OutputSystem, Task, Timer};
+use crate::{PromoMessage, PromoTaskCompleted, PromoTransition, PromoUpdate, Task, Timer};
 use derive_builder::*;
 
 /// Promo is a simple state machine
 /// with a timer and an output interface
-pub trait Promo<TTask, TTimer, TError>
+pub trait Promo<TTask, TTimer>
 where
     TTask: Task,
     TTimer: Timer,
 {
     /// shoudl call output.update
     /// and output.task_compelted if a task was completed!
-    fn update(
-        &mut self,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<(), TError>;
+    fn update(&mut self) -> Vec<PromoMessage<TTask>>;
 
     fn state(&self) -> PromoState;
 
-    /// Should call output.state_changed!
-    fn set_state(
-        &mut self,
-        state: PromoState,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<(), TError>;
+    /// returns the current task
+    fn task(&self) -> Option<&TTask>;
 
-    fn toggle_pause(
-        &mut self,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<bool, TError>;
+    /// returns the current timer
+    fn timer(&self) -> Option<&TTimer>;
+
+    /// Should call output.state_changed!
+    fn set_state(&mut self, state: PromoState) -> PromoMessage<TTask>;
+
+    fn toggle_pause(&mut self) -> PromoMessage<TTask>;
 
     fn is_paused(&self) -> bool {
         self.state() == PromoState::Paused
@@ -61,7 +55,7 @@ impl Default for PromoState {
 /// with a timer
 #[derive(Builder, Debug)]
 #[builder(setter(into))]
-pub struct SimplePromo<TTask, TTimer, TError>
+pub struct SimplePromo<TTask, TTimer>
 where
     TTask: Task,
     TTimer: Timer,
@@ -87,12 +81,9 @@ where
     state: PromoState, // internal state
     #[builder(default = "PromoState::default()")]
     prev_state: PromoState,
-
-    #[builder(default = "PhantomData::default()")]
-    phantom_error: PhantomData<TError>,
 }
 
-impl<TTask, TTimer, TError> Default for SimplePromo<TTask, TTimer, TError>
+impl<TTask, TTimer> Default for SimplePromo<TTask, TTimer>
 where
     TTask: Task,
     TTimer: Timer,
@@ -107,7 +98,7 @@ where
     }
 }
 
-impl<TTask, TTimer, TError> SimplePromo<TTask, TTimer, TError>
+impl<TTask, TTimer> SimplePromo<TTask, TTimer>
 where
     TTask: Task,
     TTimer: Timer,
@@ -128,20 +119,15 @@ where
             current_cycles: 0,
             state: PromoState::default(),
             prev_state: PromoState::default(),
-            phantom_error: PhantomData::default(),
         }
     }
 
-    fn update_working(
-        &mut self,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<(), TError> {
-        output.update(
-            self,
+    fn update_working(&mut self) -> Vec<PromoMessage<TTask>> {
+        let mut msgs = vec![PromoMessage::Update(PromoUpdate::new(
             self.state(),
             Some(&self.work_timer),
             self.tasks.get(0),
-        )?;
+        ))];
         // tick the timer
         if self.work_timer.is_completed() {
             self.current_cycles += 1;
@@ -149,124 +135,104 @@ where
             // remove first task and make it completed!
             if !self.tasks.is_empty() {
                 let comp = self.tasks.remove(0);
-                output.task_completed(self, &comp)?;
+                msgs.push(PromoMessage::TaskCompleted(PromoTaskCompleted::new(comp)));
             }
 
             // either rest or break
             if self.current_cycles == self.total_cycles {
                 // DONE!
-                self.set_state(PromoState::Completed, output)?;
+                msgs.push(self.set_state(PromoState::Completed));
             } else if self.current_cycles % self.cycles_until_rest == 0 {
-                self.set_state(PromoState::Resting, output)?;
+                msgs.push(self.set_state(PromoState::Resting));
                 self.rest_timer.reset();
             } else {
-                self.set_state(PromoState::Break, output)?;
+                msgs.push(self.set_state(PromoState::Break));
                 self.break_timer.reset();
             }
         }
-
-        Ok(())
+        msgs
     }
 
-    fn update_break(
-        &mut self,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<(), TError> {
-        output.update(
-            self,
+    fn update_break(&mut self) -> Vec<PromoMessage<TTask>> {
+        let mut msgs = vec![PromoMessage::Update(PromoUpdate::new(
             self.state(),
-            Some(&self.break_timer),
+            Some(&self.work_timer),
             self.tasks.get(0),
-        )?;
+        ))];
 
         if self.break_timer.is_completed() {
-            self.set_state(PromoState::Working, output)?;
+            msgs.push(self.set_state(PromoState::Working));
             self.work_timer.reset();
         }
-
-        Ok(())
+        msgs
     }
 
-    fn update_resting(
-        &mut self,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<(), TError> {
-        output.update(
-            self,
+    fn update_resting(&mut self) -> Vec<PromoMessage<TTask>> {
+        let mut msgs = vec![PromoMessage::Update(PromoUpdate::new(
             self.state(),
-            Some(&self.break_timer),
+            Some(&self.work_timer),
             self.tasks.get(0),
-        )?;
+        ))];
 
         if self.rest_timer.is_completed() {
-            self.set_state(PromoState::Working, output)?;
+            msgs.push(self.set_state(PromoState::Working));
             self.work_timer.start();
         }
-        Ok(())
+
+        msgs
     }
 }
 
-impl<TTask, TTimer, TError> Promo<TTask, TTimer, TError> for SimplePromo<TTask, TTimer, TError>
+impl<TTask, TTimer> Promo<TTask, TTimer> for SimplePromo<TTask, TTimer>
 where
     TTask: Task,
     TTimer: Timer,
 {
-    fn update(
-        &mut self,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<(), TError> {
+    fn update(&mut self) -> Vec<PromoMessage<TTask>> {
         match self.state() {
             PromoState::NotStarted => {
                 // start the timer and change state
                 self.work_timer.reset();
-                self.set_state(PromoState::Working, output)?;
+                vec![self.set_state(PromoState::Working)]
             }
-            PromoState::Working => self.update_working(output)?,
-            PromoState::Break => self.update_break(output)?,
-            PromoState::Resting => self.update_resting(output)?,
+            PromoState::Working => self.update_working(),
+            PromoState::Break => self.update_break(),
+            PromoState::Resting => self.update_resting(),
             PromoState::Paused | PromoState::Completed => {
-                output.update(self, self.state(), None, None)?
+                vec![PromoMessage::Update(PromoUpdate::new(
+                    self.state(),
+                    None,
+                    None,
+                ))]
             }
         }
-
-        Ok(())
     }
 
     /// Should call output.state_changed!
-    fn set_state(
-        &mut self,
-        state: PromoState,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<(), TError> {
+    fn set_state(&mut self, state: PromoState) -> PromoMessage<TTask> {
         self.prev_state = self.state();
         self.state = state;
-        output.state_changed(self, self.prev_state, self.state())?;
-        Ok(())
+        PromoMessage::Transition(PromoTransition::new(self.prev_state, self.state()))
     }
 
-    fn toggle_pause(
-        &mut self,
-        output: &mut dyn OutputSystem<TTask, TTimer, TError>,
-    ) -> Result<bool, TError> {
+    fn toggle_pause(&mut self) -> PromoMessage<TTask> {
         if !self.is_paused() {
-            match self.state {
+            match self.state() {
                 PromoState::Working => self.work_timer.pause(),
                 PromoState::Break => self.break_timer.pause(),
                 PromoState::Resting => self.rest_timer.pause(),
                 _ => (),
             }
-            self.set_state(PromoState::Paused, output)?;
+            self.set_state(PromoState::Paused)
         } else {
-            self.set_state(self.prev_state, output)?;
-            match self.state {
+            match self.prev_state {
                 PromoState::Working => self.work_timer.resume(),
                 PromoState::Break => self.break_timer.resume(),
                 PromoState::Resting => self.rest_timer.resume(),
                 _ => (),
             }
+            self.set_state(self.prev_state)
         }
-
-        Ok(self.is_paused())
     }
 
     fn is_paused(&self) -> bool {
@@ -282,13 +248,13 @@ where
 mod tests {
     use std::time::Duration;
 
-    use crate::{InstantTimer, SampleOutputData, SampleOutputSystem, SimpleTask};
+    use crate::{InstantTimer, SimpleTask};
 
     use super::*;
 
     #[test]
     fn it_should_update_states_in_order() {
-        let mut promo = SimplePromoBuilder::<SimpleTask, InstantTimer, ()>::default()
+        let mut promo = SimplePromoBuilder::<SimpleTask, InstantTimer>::default()
             .break_timer(InstantTimer::new(Duration::from_millis(100)))
             .work_timer(InstantTimer::new(Duration::from_millis(200)))
             .rest_timer(InstantTimer::new(Duration::from_millis(250)))
@@ -299,12 +265,9 @@ mod tests {
             ])
             .build()
             .unwrap();
-        let mut output = SampleOutputSystem::default();
 
         // not started
-        assert!(output.data.is_empty());
-
-        promo.update(&mut output).unwrap();
+        let output = promo.update();
 
         // start working
         assert_eq!(
